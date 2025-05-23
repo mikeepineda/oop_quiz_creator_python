cffed5402ae2c1e8bc45bbe68c8b00bf6abab86d
incessmikee <incess.mikee@gmail.com>
2025-05-23 17:15:30 +0800

    create class and initiliaze quiz question

--- quiz_creator.py
+++ quiz_creator.py
@@ -2,3 +2,11 @@
 # 1. Upload your source code to new github repository using gitbash.
 # 2. No demo needed.
 
+import json
+import os
+
+class QuizQuestion:
+    def __init__(self, question_text, options, correct_answer):
+        self.question_text = question_text
+        self.options = options
+        self.correct_answer = correct_answer
